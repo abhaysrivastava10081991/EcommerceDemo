018485c10abf785ea30213d88b8507e880d55720
Abhay Srivastava <abhaygulshan466@gmail.com>
2025-05-24 18:21:58 +0530

    adding new model

---
+++ EcommerceDemo/Models/Category.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceDemo.Models
+{
+    /// <summary>
+    /// Represents a category of products in the e-commerce application.
+    /// </summary>
+    public class Category
+    {
+        /// <summary>
+        /// Primary key for the category.
+        /// </summary>
+        [Key]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Name of the category.
+        /// </summary>
+        [Required]
+        [DisplayName("Category Name")]
+        public string? Name { get; set; }
+        /// <summary>
+        /// Description of the Display Order.
+        /// </summary>
+        [DisplayName("Display Order")]
+        [Range(1,1000)]
+        public int DisplayOrder { get; set; }
+    }
+}
+
